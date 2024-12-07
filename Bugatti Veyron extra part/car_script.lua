f2fa169e360b2d964ca320dfd982ac258c6fbcc3
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2024-12-07 04:47:46 +0100

    s

--- Bugatti Veyron extra part/car_script.lua
+++ Bugatti Veyron extra part/car_script.lua
@@ -1,5 +1,8 @@
 local obj_script = {}
 
+obj_script.off_light = Color(0, 0, 0)
+obj_script.red_light = Color(255, 0, 0)
+
 obj_script.init = function(name, obj, bone, variable, genid)
 end
 
@@ -9,6 +12,13 @@ obj_script.update = function(name, obj, bone, variable, genid)
     //bone return the bones entity
     //variable return local variable setup inside of updateVariable()
     //genid return an id between 1 and "count" parameter
+	
+	
+	/* BACK_LIGHT */
+    if name == "red_back_light" then
+        obj:setColor(variable.brake and obj_script.red_light or obj_script.off_light)
+    end
+    /* */
 end
 
 return obj_script
