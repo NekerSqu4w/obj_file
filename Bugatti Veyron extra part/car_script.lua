4a0cc6477e39d811137c461ca66819f834f8ffec
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2024-12-07 01:18:55 +0100

    ye

---
+++ Bugatti Veyron extra part/car_script.lua
@@ -0,0 +1,14 @@
+local obj_script = {}
+
+obj_script.init = function(name, obj, bone, variable, genid)
+end
+
+obj_script.update = function(name, obj, bone, variable, genid)
+    //name return current model name
+    //obj return the current obj entity
+    //bone return the bones entity
+    //variable return local variable setup inside of updateVariable()
+    //genid return an id between 1 and "count" parameter
+end
+
+return obj_script
