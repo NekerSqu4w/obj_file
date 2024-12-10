1b99ac53b340edd5b888ca128966eb9d9779faef
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2024-12-10 02:05:12 +0100

    Update needles_script.lua

--- Bugatti Veyron extra part/needles_script.lua
+++ Bugatti Veyron extra part/needles_script.lua
@@ -16,64 +16,64 @@ obj_script.update = function(name, obj, bone, variable, genid)
 	
 	// hour needle
     if name == "hour_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.hour_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.hour_ang,0,0)))
     end
     if name == "hour_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.hour_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.hour_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 	
 	// minute needle
     if name == "minute_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.minute_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.minute_ang,0,0)))
     end
     if name == "minute_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.minute_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.minute_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 	
 	// rpm needle
     if name == "rpm_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.rpm_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.rpm_ang,0,0)))
     end
     if name == "rpm_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.rpm_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.rpm_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 	
 	// speed needle
     if name == "speed_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.speed_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.speed_ang,0,0)))
     end
     if name == "speed_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.speed_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.speed_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 	
 	// power needle
     if name == "power_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.power_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.power_ang,0,0)))
     end
     if name == "power_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.power_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.power_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 	
 	// temp needle
     if name == "temp_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.temp_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.temp_ang,0,0)))
     end
     if name == "temp_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.temp_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.temp_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 	
 	// fuel needle
     if name == "fuel_needle" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.fuel_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.fuel_ang,0,0)))
     end
     if name == "fuel_needle_red_part" then
-		obj:setAngles(bone:localToWorldAngles(Angle(variable.fuel_ang,0,0)))
+		obj:setAngles(bone:localToWorldAngles(Angle(-variable.fuel_ang,0,0)))
         obj:setColor(variable.headlight > 0 and obj_script.on or obj_script.off)
     end
 end
